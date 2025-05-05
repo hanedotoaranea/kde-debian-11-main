c5555f0a85dd25a80e28e759f345a18e64fdf54a
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-05-06 00:09:46 +0500

    Update build.sh
    Signed-off-by: hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 lb config -d bullseye \
           --debian-installer live \
-          --debian-installer-distribution bullseye \ 
+          --debian-installer-distribution bullseye \
           --debian-installer-gui true \
-          --iso-publisher "hanedotoaranea <https://github.com/hanedotoaranea>" \
           --archive-areas "main contrib non-free" \
           --debootstrap-options "--variant=minbase" \
           
