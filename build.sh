8f110d0ce503ea94b4a93b1ff3102abae3d639fb
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-05-05 23:56:48 +0500

    Update build.sh
    Signed-off-by: hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 lb config -d bullseye \
           --debian-installer live \
-          --debian-installer-distribution bullseye \
+          --debian-installer-distribution bullseye \ 
+          --iso-application "KDE Debian 11" \
           --debian-installer-gui true \
+          --iso-publisher "hanedotoaranea <https://github.com/hanedotoaranea>" \
           --archive-areas "main contrib non-free" \
           --debootstrap-options "--variant=minbase" \
-          
+          --iso-volume "KDE Debian 11" \
 sudo lb build
